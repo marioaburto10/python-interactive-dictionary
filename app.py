885e73f9f8bf0fdef6e60069d9e2c1199750b920
Mario Aburto <marioaburto10@gmail.com>
2018-08-06 22:21:39 -0400

    added an if statement to check to see if the user's input word does exist in the dictioanry or not

--- app.py
+++ app.py
@@ -7,9 +7,15 @@ data = json.load(open("./Resources/data.json"));
 
 # a function that takes in a word as a string and returns that word's definition if it is in the data.json file 
 def translate(word): 
-	return data[word]
+	# an if statement that checks if the word does exist in the dictioany in data.json
+	# if the word does exist in the dictionary, return its definition
+	# else return a string saying it does not exist
+	if word in data:
+		return data[word]
+	else:
+		return "word does not exist, please try another word"
 
 # will ask the user to input a word and it will be stored in the variable called word
 word = input("Enter a word that you want a definition for: ") 
 
-translate(word) # if word = "hello", it will return ['Expression of greeting used by two or more people who meet each other.']
+print(translate(word)) # if word = "hello", it will return ['Expression of greeting used by two or more people who meet each other.']

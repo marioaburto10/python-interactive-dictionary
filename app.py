49b788b19cabfdd6cd9ac0e7a70dcf93ac68f099
Mario Aburto <marioaburto10@gmail.com>
2018-08-06 23:56:07 -0400

    if the word returns more than one definition return the different definitions in order, else return the only definition

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ def translate(word):
 	# turns the user's word to all lowercase letters just incase the urser uses a micture of upercase and lower case letters
 	word = word.lower()
 	# an if statement that checks if the word does exist in the dictioany in data.json
+	
 	# if the word does exist in the dictionary, return its definition
-
 	if word in data:
 		return data[word]
 	# if the word is not the data.json file, then sue the get_close_matches function from difflib to check for any similar words
@@ -20,18 +20,26 @@ def translate(word):
 		answer = input("Did you mean %s instead? Enter Y for yes or N for no: " % get_close_matches(word, data.keys())[0])
 		answer = answer.lower()
 
-
+		# if the user's answer is y or if they just hit the Enter key, then display the suggested word's definition
 		if answer == "" or answer[0] == 'y':
 			recomended_word = get_close_matches(word, data.keys())[0]
 			return data[recomended_word]
+		# if the suggested word is not correct then tell the user that no macthes were found
 		elif answer[0] == "n":
-			return "Sorry, no matches found. Please try another word"
+			return "Sorry, no matches found. Please try another word."
 		
-	# else return a string saying it does not exist
+	# if the word is not in the dictionary or if there are no similar matches found return a string saying it does not exist
 	else:
-		return "Word does not exist in this dictionary, please try another word"
+		return "Word does not exist in this dictionary, please try another word."
 
 # will ask the user to input a word and it will be stored in the variable called word
 word = input("Enter a word that you want a definition for: ") 
 
-print(translate(word)) # if word = "hello", it will return ['Expression of greeting used by two or more people who meet each other.']
+# variable to hold the output of our translate function
+output = translate(word) # if word = "hello", it will return ['Expression of greeting used by two or more people who meet each other.']
+
+if type(output) == list:
+	for i, definition in enumerate(output):
+		print(str(i + 1) + ".) " + definition)
+
+

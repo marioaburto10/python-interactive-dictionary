dbe5be0d3543546b2439a72088a0d443c389babb
Mario Aburto <marioaburto10@gmail.com>
2018-08-06 23:46:34 -0400

    added if statement tha will check the user's answer and check it with any similar words if the word was not found in the dictionary

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # importing my dependencies
 import json
+from difflib import get_close_matches 
 
 # using the json load() function to open and load the data.json file
 # data is a dictionary
@@ -7,13 +8,28 @@ data = json.load(open("./Resources/data.json"));
 
 # a function that takes in a word as a string and returns that word's definition if it is in the data.json file 
 def translate(word): 
+	# turns the user's word to all lowercase letters just incase the urser uses a micture of upercase and lower case letters
+	word = word.lower()
 	# an if statement that checks if the word does exist in the dictioany in data.json
 	# if the word does exist in the dictionary, return its definition
-	# else return a string saying it does not exist
+
 	if word in data:
 		return data[word]
+	# if the word is not the data.json file, then sue the get_close_matches function from difflib to check for any similar words
+	elif len(get_close_matches(word, data.keys())) > 0:
+		answer = input("Did you mean %s instead? Enter Y for yes or N for no: " % get_close_matches(word, data.keys())[0])
+		answer = answer.lower()
+
+
+		if answer == "" or answer[0] == 'y':
+			recomended_word = get_close_matches(word, data.keys())[0]
+			return data[recomended_word]
+		elif answer[0] == "n":
+			return "Sorry, no matches found. Please try another word"
+		
+	# else return a string saying it does not exist
 	else:
-		return "word does not exist, please try another word"
+		return "Word does not exist in this dictionary, please try another word"
 
 # will ask the user to input a word and it will be stored in the variable called word
 word = input("Enter a word that you want a definition for: ") 
